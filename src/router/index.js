d7ac1ea221e6de11c160e9badbb863690b194d3f
bui.thi.thuy.dung <FRAMGIA\bui.thi.thuy.dung@b121536-lt.framgia.com>
2019-04-24 16:30:05 +0700

    CRUD

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import Router from 'vue-router'
 import Login from '@/components/Login'
 import Home from '@/components/Home'
 import Signup from '@/components/Signup'
+import CompanyIndex from '@/components/companies/CompanyIndex'
+import CompanyCreate from '@/components/companies/CompanyCreate'
+import CompanyEdit from '@/components/companies/CompanyEdit'
 
 Vue.use(Router)
 
@@ -23,6 +26,21 @@ export default new Router({
       path: '/signup',
       name: 'Signup',
       component: Signup
+    },
+    {
+      path: '/companies',
+      name: 'CompanyIndex',
+      component: CompanyIndex,
+    },
+    {
+      path: '/companies/create',
+      component: CompanyCreate,
+      name: 'createCompany'
+    },
+    {
+      path: '/companies/edit/:id',
+      component: CompanyEdit,
+      name: 'editCompany'
     }
   ]
 })
